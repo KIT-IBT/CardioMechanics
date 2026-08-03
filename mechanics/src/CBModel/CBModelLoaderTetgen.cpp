df49826838ce3d7f457c857224008a860f4da2dd
Tobias Gerach <142993154+tobias-gerach@users.noreply.github.com>
2026-08-03 14:14:22 +0200

    fix issue #13

--- mechanics/src/CBModel/CBModelLoaderTetgen.cpp
+++ mechanics/src/CBModel/CBModelLoaderTetgen.cpp
@@ -49,9 +49,9 @@ CBModel *CBModelLoaderTetgen::Load(std::string nodesFilename, std::string elemen
     model_->ExtractSolidElements();
     Base::model_->ExtractSurfaceElements();
     
-    Base::model_->DetermineNeighbors();
-    
-
+    if (determineNeighbors){
+        Base::model_->DetermineNeighbors();
+    }
     
     return Base::model_;
 }
